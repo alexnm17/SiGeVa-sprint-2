4bbce7ec4f7937ada04aed98251eab3b8e5fa106
Jaime León <jaimeleonro@gmail.com>
2021-10-22 12:27:25 +0200

    Creacion de CentroVacunacionDao

---
+++ src/main/java/edu/esi/uclm/dao/CentroVacunacionDao.java
@@ -0,0 +1,11 @@
+package edu.esi.uclm.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.esi.uclm.model.CentroVacunacion;
+
+@Repository
+public interface CentroVacunacionDao extends MongoRepository<CentroVacunacion, String> {
+
+}
