d3cc22169fcfdd879c3a27adb6e70bf67856f5a5
Jaime León <jaimeleonro@gmail.com>
2021-10-22 15:29:54 +0200

    Cambios para evitar conflicto al hacer merge Jaime -> master

--- src/main/java/edu/esi/uclm/model/Sanitario.java
+++ src/main/java/edu/esi/uclm/model/Sanitario.java
@@ -2,6 +2,8 @@ package edu.esi.uclm.model;
 
 public class Sanitario extends Usuario{
 
-	public Sanitario(){}
+	public Sanitario(){
+		//El constructor vacio ha sido crado por exigencias del Spring
+	}
 	
 }
