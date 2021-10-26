295ec1810f7671e5090f6c6ed4c860ba26bea555
JAIME LEON ROSADO <Jaime.Leon2@alu.uclm.es>
2021-10-26 22:07:20 +0000

    Revert "Creación del método para la definición del formato de vacunación"

--- src/main/java/edu/esi/uclm/http/FormatoVacunacionController.java
+++ src/main/java/edu/esi/uclm/http/FormatoVacunacionController.java
@@ -1,39 +1,11 @@
 package edu.esi.uclm.http;
 
 
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import edu.esi.uclm.dao.FormatoVacunacionDao;
-import edu.esi.uclm.model.FormatoVacunacion;
 
 @RestController
 public class FormatoVacunacionController {
 
-	@Autowired
-	private FormatoVacunacionDao formatoVacunacionDao;
-	
-	@PostMapping("/definirFormatoVacunacion")
-	public void definirFormatoVacunacion(HttpSession session, @RequestBody Map<String, Object> datosFormatoVacunacion) {
-		JSONObject jso = new JSONObject(datosFormatoVacunacion);
-		
-		String horaInicio = jso.getString("horaIncio");
-		String horaFin = jso.getString("horaFin");
-		String duracionFranja = jso.getString("duracionFranja");
-		int personasAVacunar = jso.getInt("personasAVacunar");
-		
-		FormatoVacunacion formatoVacunacion = new FormatoVacunacion(horaInicio, horaFin, duracionFranja, personasAVacunar);
-		formatoVacunacionDao.insert(formatoVacunacion);
-		
-	}
-	
 	public void setPersonalVacunacion() {
 		
 	}
