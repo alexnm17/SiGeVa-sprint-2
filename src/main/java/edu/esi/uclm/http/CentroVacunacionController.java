16ef55a34d3ef4b814cf46a860539ef0a9246f11
Alejandro <Alejandro.Nieto3@alu.uclm.es>
2021-10-27 13:47:16 +0200

    Creacion del Método Crear Centro de Vacunación

--- src/main/java/edu/esi/uclm/http/CentroVacunacionController.java
+++ src/main/java/edu/esi/uclm/http/CentroVacunacionController.java
@@ -1,9 +1,33 @@
 package edu.esi.uclm.http;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import edu.esi.uclm.dao.CentroVacunacionDao;
+import edu.esi.uclm.model.CentroVacunacion;
+
 
 @RestController
 public class CentroVacunacionController {
 	
+	@Autowired
+	private CentroVacunacionDao centroDao;
+	
+	@PostMapping("/addCentro")
+	public void add(HttpServletRequest request,@RequestBody CentroVacunacion centro) {
+		try {
+			centroDao.save(centro);
+		} catch(Exception e) {	
+			throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+		}
+	}
 
 }
