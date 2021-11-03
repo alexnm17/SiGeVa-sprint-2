9789f2a48e4b7c7bb48bab97002c2fae12c00b1c
manumorales98 <manumr1998@gmail.com>
2021-11-03 18:11:42 +0100

    prueba 1

---
+++ src/webapp/frontend/src/components/login.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Breadcrumb, Button } from "react-bootstrap"
+import axios from "axios";
+ 
+
+class Login {
+		constructor() {
+			
+		}
+
+		login() {
+		}
+		
+		recoverPwd() {
+			};
+		}
+		
+		register() {
+		}
+
+		connected() {
+			
+		}
+
+		disconnected() {
+			
+		}
+
+		transitionCompleted() {
+		}
+	}
+
+	return Login;
+;
