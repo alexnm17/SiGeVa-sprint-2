be78f38eeb281f00e14a6296d27bd47746aefa5e
Alejandro <Alejandro.Nieto3@alu.uclm.es>
2021-11-08 15:50:55 +0100

    Metodo modificar centro de vacunacion

--- src/main/java/edu/esi/uclm/http/UsuarioController.java
+++ src/main/java/edu/esi/uclm/http/UsuarioController.java
@@ -26,9 +26,11 @@ import edu.uclm.esi.exceptions.SiGeVaException;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import edu.esi.uclm.dao.CentroVacunacionDao;
 import edu.esi.uclm.dao.CitaDao;
 import edu.esi.uclm.dao.UsuarioDao;
 import edu.esi.uclm.exceptions.SigevaException;
+import edu.esi.uclm.model.CentroVacunacion;
 import edu.esi.uclm.model.EstadoVacunacion;
 import edu.esi.uclm.model.RolUsuario;
 
@@ -39,6 +41,8 @@ public class UsuarioController {
 	private UsuarioDao usuarioDao;
 	@Autowired
 	private CitaDao citaDao;
+	@Autowired
+	private CentroVacunacionDao centroDao;
 
 	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping("/crearUsuario")
@@ -94,23 +98,40 @@ public class UsuarioController {
 
 	}
 
+
+	@PostMapping("/modificarCentro")
+	public void modificarCentro(@RequestBody CentroVacunacion centro) {
+		try {
+			CentroVacunacion antiguoCentro = centroDao.findByNombre(centro.getNombre());
+
+			if (antiguoCentro == null)
+				throw new SiGeVaException(HttpStatus.NOT_FOUND, "No existe un centro con este nombre");
+			
+			antiguoCentro.setMunicipio(centro.getMunicipio());
+			centroDao.save(antiguoCentro);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+		}
+
+	}
+
 	@PostMapping("/login")
-    public void login(HttpServletRequest request, HttpSession session, @RequestBody Map<String, Object> datosUsuario) {
-        try {
-            JSONObject jso = new JSONObject(datosUsuario);
-            String dni = jso.optString("dni");
-            String password= jso.optString("password");
-            String rol= jso.optString("rol");
-            if (dni.length()==0) throw new SigevaException(HttpStatus.FORBIDDEN, "Por favor, escribe tu DNI");
-            
-            Usuario usuario = usuarioDao.findByDniAndPasswordAndRol(dni, password, rol);
-            if (usuario==null) throw new SigevaException(HttpStatus.UNAUTHORIZED, "Credenciales inválidas");
-            request.getSession().setAttribute("dniUsuario", dni);
-            request.getSession().setAttribute("rolUsuario", rol);
-        } catch (SigevaException e) {
-        	throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
-        }
-    }
+	public void login(HttpServletRequest request, HttpSession session, @RequestBody Map<String, Object> datosUsuario) {
+		try {
+			JSONObject jso = new JSONObject(datosUsuario);
+			String dni = jso.optString("dni");
+			String password= jso.optString("password");
+			String rol= jso.optString("rol");
+			if (dni.length()==0) throw new SigevaException(HttpStatus.FORBIDDEN, "Por favor, escribe tu DNI");
+
+			Usuario usuario = usuarioDao.findByDniAndPasswordAndRol(dni, password, rol);
+			if (usuario==null) throw new SigevaException(HttpStatus.UNAUTHORIZED, "Credenciales inválidas");
+			request.getSession().setAttribute("dniUsuario", dni);
+			request.getSession().setAttribute("rolUsuario", rol);
+		} catch (SigevaException e) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+		}
+	}
 
 	@CrossOrigin(origins = "http://localhost:3000")
 	@DeleteMapping("/eliminarUsuario")
@@ -128,13 +149,13 @@ public class UsuarioController {
 
 			if(user.getRol().equals(RolUsuario.PACIENTE.name()) && !user.getEstadoVacunacion().equals(EstadoVacunacion.NO_VACUNADO.name()))
 				throw new SigevaException(HttpStatus.FORBIDDEN, "No puede eliminar a un paciente vacunado del sistema");
-			
-			
+
+
 			borrarCitas(user.getDni());
 			usuarioDao.delete(user);
-				
-			
-				
+
+
+
 		} catch (Exception e) {
 
 			throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
