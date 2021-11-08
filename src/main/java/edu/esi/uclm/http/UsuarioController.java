8092d8f84d9d4df56f330dc72d80fa6ceb8fe78b
Sergio <Sergio.perez18@alu.uclm.es>
2021-11-08 16:46:09 +0100

    Añadir controles de atributos del usuario y borrado en cascada de las citas

--- src/main/java/edu/esi/uclm/http/UsuarioController.java
+++ src/main/java/edu/esi/uclm/http/UsuarioController.java
@@ -2,6 +2,7 @@ package edu.esi.uclm.http;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -21,9 +22,11 @@ import edu.uclm.esi.exceptions.SiGeVaException;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import edu.esi.uclm.dao.CentroVacunacionDao;
 import edu.esi.uclm.dao.CitaDao;
 import edu.esi.uclm.dao.UsuarioDao;
 import edu.esi.uclm.exceptions.SigevaException;
+import edu.esi.uclm.model.CentroVacunacion;
 import edu.esi.uclm.model.EstadoVacunacion;
 import edu.esi.uclm.model.RolUsuario;
 
@@ -34,26 +37,30 @@ public class UsuarioController {
 	private CitaDao citaDao;
 	@Autowired
 	private UsuarioDao usuarioDao;
+	@Autowired
+	private CentroVacunacionDao centroVacunacionDao;
 
 	@CrossOrigin(origins = "https://bold-spaceship-775486.postman.co")
 	@PostMapping("/login")
-    public void login(HttpServletRequest request, HttpSession session, @RequestBody Map<String, Object> datosUsuario) {
-        try {
-            JSONObject jso = new JSONObject(datosUsuario);
-            String dni = jso.optString("dni");
-            String password= jso.optString("password");
-            String rol= jso.optString("rol");
-            if (dni.length()==0) throw new SigevaException(HttpStatus.FORBIDDEN, "Por favor, escribe tu DNI");
-            
-            Usuario usuario = usuarioDao.findByDniAndPasswordAndRol(dni, password, rol);
-            if (usuario==null) throw new SigevaException(HttpStatus.UNAUTHORIZED, "Credenciales inválidas");
-            request.getSession().setAttribute("dniUsuario", dni);
-            request.getSession().setAttribute("rolUsuario", rol);
-        } catch (SigevaException e) {
-        	throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
-        }
-    }
-	
+	public void login(HttpServletRequest request, HttpSession session, @RequestBody Map<String, Object> datosUsuario) {
+		try {
+			JSONObject jso = new JSONObject(datosUsuario);
+			String dni = jso.optString("dni");
+			String password = jso.optString("password");
+			String rol = jso.optString("rol");
+			if (dni.length() == 0)
+				throw new SigevaException(HttpStatus.FORBIDDEN, "Por favor, escribe tu DNI");
+
+			Usuario usuario = usuarioDao.findByDniAndPasswordAndRol(dni, password, rol);
+			if (usuario == null)
+				throw new SigevaException(HttpStatus.UNAUTHORIZED, "Credenciales inválidas");
+			request.getSession().setAttribute("dniUsuario", dni);
+			request.getSession().setAttribute("rolUsuario", rol);
+		} catch (SigevaException e) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+		}
+	}
+
 	@PostMapping("/crearUsuario")
 	public void crearUsuario(@RequestBody Map<String, Object> datosUsuario) {
 
@@ -68,6 +75,7 @@ public class UsuarioController {
 			String estadoPaciente = "No Vacunado";
 			String rol = json.getString("rol");
 			Usuario nuevoUsuario = new Usuario(dni, nombre, apellido, password, rol, centroSalud, estadoPaciente);
+			ComprobarCentro(centroSalud);
 			nuevoUsuario.controlarContraseña();
 			nuevoUsuario.comprobarDni();
 			usuarioDao.save(nuevoUsuario);
@@ -83,15 +91,14 @@ public class UsuarioController {
 			if (user.getRol().equals(RolUsuario.ADMINISTRADOR.name()))
 				throw new SigevaException(HttpStatus.FORBIDDEN, "No puede modificar a otro administrador del sistema");
 			else {
-
 				Usuario antiguoUsuario = usuarioDao.findByDni(user.getDni());
-
 				if (antiguoUsuario == null)
 					throw new SiGeVaException(HttpStatus.NOT_FOUND, "No existe un usuario con este identificador");
 				antiguoUsuario.setNombre(user.getNombre());
 				antiguoUsuario.setApellido(user.getApellido());
 				if (!antiguoUsuario.getCentroSalud().equals(user.getCentroSalud()))
-					antiguoUsuario.comprobarEstado();
+					ComprobarCentro(user.getCentroSalud());
+				antiguoUsuario.comprobarEstado();
 				antiguoUsuario.setCentroSalud(user.getCentroSalud());
 				antiguoUsuario.setPassword(user.getPassword());
 
@@ -99,7 +106,6 @@ public class UsuarioController {
 				usuarioDao.save(antiguoUsuario);
 
 			}
-
 		} catch (Exception e) {
 
 			throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
@@ -125,7 +131,7 @@ public class UsuarioController {
 			if (user.getRol().equals("Paciente"))
 				user.comprobarEstado();
 
-			BorrarCitas(dni);
+			borrarCitas(dni);
 			usuarioDao.delete(user);
 
 		} catch (Exception e) {
@@ -134,7 +140,7 @@ public class UsuarioController {
 		}
 	}
 
-	private void BorrarCitas(String dni) {
+	private void borrarCitas(String dni) {
 		citaDao.deleteByUsuarioDni(dni);
 	}
 
@@ -163,6 +169,13 @@ public class UsuarioController {
 	@GetMapping("/getUsuarios")
 	public List<Usuario> getUsuarios(HttpSession session) {
 		return usuarioDao.findAll();
+	}
+
+	private void ComprobarCentro(String centroSalud) throws SiGeVaException {
+		CentroVacunacion centro = centroVacunacionDao.findByNombre(centroSalud);
+		if (centro==null)
+			throw new SiGeVaException(HttpStatus.NOT_FOUND, "No existe el centro de salud introducido");
+
 	}
 
 }
